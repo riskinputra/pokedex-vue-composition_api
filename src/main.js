6ef26019f4cf8e27cfe08a3cac6d55e48ce85a2d
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2021-10-16 13:21:07 +0700

    adding tailwind

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Styles
+import './styles/tailwind.css'
+
+// Modules
 import homeModule from './modules/home'
 
 import { registerModules } from './register-modules'
